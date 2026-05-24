3a732785ec0763095c7d48ee470438c34422cbad
promediarecords <promediabest7@gmail.com>
2026-05-24 23:01:43 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='shiloh-whispers-v10';
+self.addEventListener('install',event=>{self.skipWaiting()});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',event=>{});
+self.addEventListener('notificationclick',event=>{event.notification.close();event.waitUntil(clients.openWindow('/'))});
