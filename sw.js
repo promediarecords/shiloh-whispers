d9f1dfc510f43364abea5f0221bd8ea25e5e8135
promediarecords <promediabest7@gmail.com>
2026-05-24 17:41:36 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='shiloh-whispers-v11';
-self.addEventListener('install',e=>{self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{});
-self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.openWindow('/'))});
